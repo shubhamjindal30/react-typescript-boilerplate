ff9fbd8daba555b66b55e7e7fa1c85e8c747aa07
Shubham Jindal <shubhamjindal1234@gmail.com>
2021-05-10 18:27:06 +1000

    feat: setup router, cleanup project

---
+++ src/views/common/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing: React.FunctionComponent = () => {
+  return (
+    <div>
+      <p>This is the landing page.</p>
+    </div>
+  );
+};
+
+export default Landing;
---
+++ src/views/index.ts
@@ -0,0 +1,3 @@
+import Landing from './common/Landing';
+
+export { Landing };
